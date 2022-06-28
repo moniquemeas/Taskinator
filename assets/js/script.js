bb489cc98372b1a35b0ae88a0a034517bd18dcf1
Monique <monique.meas@gmail.com>
2022-06-28 04:23:11 -0400

    reset the from

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,19 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value ;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
   
- 
+ // check if input value are empty strings
+
+ if(!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+ }
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     };
     // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
+  formEl.reset();
 };
   
 
